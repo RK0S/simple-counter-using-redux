74a7608e34f4694bdd9efb8b25aa0637e38c4478
Roman <example@gmail.com>
2023-02-09 13:40:44 +0300

    create simple counter using redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.count)
+
+    function inc() {
+        dispatch({type: 'ADD_ONE', payload: 1})
+    }
+
+    function dec() {
+        dispatch({type: 'REMOVE_ONE', payload: 1})
+    }
     return (
         <div className="App">
-            
+            <div>{count}</div>
+            <button onClick={() => inc()} >+</button>
+            <button onClick={() => dec()}>-</button>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+
+const defaultState = {
+    count: 0
+}
+
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'ADD_ONE':
+            return {...state, count: state.count + action.payload}
+        case 'REMOVE_ONE':
+            return {...state, count: state.count - action.payload}
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
